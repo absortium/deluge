96ca1780f7f90d7d487b09c1c21899e694ac1fc8
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-04-10 12:43:41 +0300

    change usefull aliases

--- useful/aliases/deluge.sh
+++ useful/aliases/deluge.sh
@@ -1,15 +1,26 @@
+#!/usr/bin/env bash
 # ------------------------------------
 # Deluge alias and function
 # ------------------------------------
 
+export DELUGE_DIR="$PWD/PycharmProjects/deluge/"
+
 # Go to the deluge directory
-alias god="cd $(PWD)/PycharmProjects/deluge/"
+alias god="cd $DELUGE_DIR"
 
 # Go to the deluge docker dev directory
-alias godd="cd $(PWD)/PycharmProjects/deluge/docker/dev"
+alias godd="cd $DELUGE_DIR/docker/dev"
 
 # Go to the deluge services directory
-alias gods="cd $(PWD)/PycharmProjects/deluge/services"
+alias gods="cd $DELUGE_DIR/services"
 
 # Init deluge project
-alias ideluge="source $(PWD)/PycharmProjects/deluge/environments/secrets.sh"
+ideluge() {
+    source "$DELUGE_DIR/environments/secrets.sh"
+}
+
+# Delete crossbar process files if you forgot to shutdown router gracefully
+cleancrossbar() {
+    rm "$DELUGE_DIR/services/router/.crossbar/node.key"
+    rm "$DELUGE_DIR/services/router/.crossbar/node.pid"
+}
