c237ebd87f216045972560d10b1248e3935b8232
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-04-14 14:32:32 +0300

    change aliases; add worker service (celery); add task execution in django signals

--- useful/aliases/docker..sh
+++ useful/aliases/docker..sh
@@ -19,13 +19,13 @@ alias di="docker images"
 alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
 
 # Run deamonized container, e.g., $dkd base /bin/echo hello
-alias dkd="docker run -d -P"
+alias drund="docker run -d -P"
 
 # Run interactive container, e.g., $dki base /bin/bash
-alias dki="docker run -i -t -P"
+alias drun="docker run -i -t -P"
 
 # Execute command in container, e.g., $dex base /bin/bash
-alias dex="docker exec -i -t"
+alias dexe="docker exec -i -t"
 
 func_dstop() { docker stop $(docker ps -a -q); }
 # Stop all containers
@@ -34,13 +34,13 @@ alias dstop=func_dstop
 func_drmc() { docker ps -a | egrep "$1" | grep -v "CONTAINER" | awk '{ printf "%s\n", $1}' | xargs docker rm -f; }
 # Stop and Remove choisen containers
 # drmc "postgres" - delete postgres container
-# drmc "[a-z]" - delete all containers
+# drmc ".*" - delete all containers
 alias drmc=func_drmc
 
 func_drmi() { docker images -a | egrep "$1" | grep -v "IMAGE" | awk '{ printf "%s\n", $3}' | xargs docker rmi -f; }
 # Remove choisen images
 # drmi "postgres" - delete postgres image
-# drmi "[a-z]" - delete all images
+# drmi ".*" - delete all images
 alias drmi=func_drmi
 
 func_dbu() { docker build -t=$1 .; }
