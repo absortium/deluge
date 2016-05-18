ab3a6b10330a84c8aeca5ab4da18ded973b002ea
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-05-18 13:30:17 +0300

    delete ethwallet and create separate repository

--- services/ethclient/ethclient/crossbarhttp/__init__.py
+++
@@ -1,8 +0,0 @@
-from ethclient.crossbarhttp.client import (get_crossbar_client,
-                                           ClientBadHost,
-                                           ClientBadUrl,
-                                           ClientMissingParams,
-                                           ClientCallRuntimeError,
-                                           ClientBaseException,
-                                           ClientNoCalleeRegistered,
-                                           ClientSignatureError)
