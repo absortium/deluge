e06c45cf5a73f92d0bec5cf8c8f7cdf2289ef0f4
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-07-12 20:31:19 +0300

    add swith case in install.sh for different services

--- useful/install.sh
+++ useful/install.sh
@@ -1,30 +1,80 @@
 #!/bin/bash
 
-set -ev
+set -e
+shopt -s expand_aliases
 
 declare SERVICE="$1"
-echo "$SERVICE"
+
+declare GREEN='\033[0;32m'
+declare NC='\033[0m'
+print () {
+    echo -e "${GREEN}$1${NC}"
+}
 
 export DELUGE_PATH="$PWD"
 
 # For simplicity I prefer use aliases which I developed for this project, on first sign it might look overwhelming, but
 # I think it may significantly help for developing.
-echo "Step #0: Install aliases."
+print "Step #0: Install aliases."
 for f in $DELUGE_PATH/useful/aliases/*; do
-  source "$f"
+    echo  "Install aliases from: $f"
+    source "$f"
 done
 
-echo "Step #1: Turn on 'unit' mode (is using for testing)."
+print  "Step #1: Turn on 'unit' mode."
 dcinit unit
 
-echo "Step #2: Install and run 'postgres' service."
-dc up -d postgres
+case "$SERVICE" in
+    'frontend' )
+        print "Step #2: Build 'base-frontend' service."
+        dc build base-frontend
 
-echo "Step #3: Build 'base-backend' service."
-dc build base-backend
+        print "Step #3: Install frontend and run tests."
+        dc run frontend run test
 
-echo "Step #4: Build 'backend' service."
-dc build backend
+        ;;
 
-echo "Step #5: Install frontend and run tests."
-dc run m-backend test --verbosity 2 absortium.tests.unit
+    'backend' )
+        print "Step #2: Install and run 'postgres' service."
+        dc up -d postgres
+
+        print "Step #3: Build 'base-backend' service."
+        dc build base-backend
+
+        print "Step #4: Build 'backend' service."
+        dc build backend
+
+        print "Step #5: Migrate database."
+        dc run m-backend migrate
+
+        print "Step #6: Run tests."
+        dc run m-backend test --verbosity 2 absortium.tests.unit
+
+        ;;
+
+    'ethwallet' )
+        print "Step #2: Install and run 'postgres' service."
+        dc up -d postgres
+
+        print "Step #3: Build 'base-backend' service."
+        dc build base-ethwallet
+
+        print "Step #4: Build 'backend' service."
+        dc build ethwallet
+
+        print "Step #5: Create 'ethwallet' database."
+        dex postgres psql -c "CREATE DATABASE ethwallet" -U postgres
+
+        print "Step #6: Migrate database."
+        dc run m-ethwallet migrate
+
+        print "Step #7: Run tests."
+        dc run m-ethwallet test --verbosity 2 ethwallet.tests.unit
+
+        ;;
+
+    *)
+        print "Can't find any services similar to '$1'"
+        break
+esac
+
