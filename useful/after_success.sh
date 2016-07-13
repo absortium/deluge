14ce3202df306a25253ff53a4143f948accfbf4f
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-07-13 21:08:32 +0300

    fix docker login

--- useful/after_success.sh
+++ useful/after_success.sh
@@ -9,27 +9,26 @@ print () {
     echo -e "${GREEN}$1${NC}"
 }
 
-declare DOCKER_EMAIL="$1"
-declare DOCKER_USER="$2"
-declare DOCKER_PASS="$3"
-declare SERVICE="$4"
-declare BRANCH="$5"
+declare DOCKER_USER="$1"
+declare DOCKER_PASS="$2"
+declare SERVICE="$3"
+declare BRANCH="$4"
 
 echo "SERVICE=$SERVICE"
 echo "BRANCH=$BRANCH"
 
 print "Step #0: Install aliases."
-for f in "$DELUGE_PATH/useful/aliases/*"; do
+for f in $DELUGE_PATH/useful/aliases/*; do
     echo "Install aliases from: $f"
     source "$f"
 done
 
 if [ -n "$BRANCH" ]; then
     print "Step #1 Login to the DockerHub"
-    docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
+    docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
     if [ "$BRANCH" == "master" ]; then
-        declare IMAGE="absortium/$SERVICE"
+        declare IMAGE="$SERVICE"
 
         print "Step #2: Switch to the 'realnet' mode."
         dcinit realnet
@@ -38,12 +37,16 @@ if [ -n "$BRANCH" ]; then
         dc build frontend
 
     elif [ "$BRANCH" == "development" ]; then
-        declare IMAGE="absortium/base-$SERVICE"
+        declare IMAGE="base-$SERVICE"
 
-        print "Step #2: We already in 'unit' mode."
-        print "Step #3: Image $IMAGE already built."
+        print "Step #2: Switch to the 'unit' mode."
+        dcinit unit
+
+        print "Step #3: Image $IMAGE should be already built."
     fi
 
-    print "Step #2 Push image - $IMAGE on docker hub"
-    docker push "$IMAGE"
+    if [ -n "$IMAGE" ]; then
+        print "Step #4: Push '$IMAGE' image on docker hub"
+        docker push "absortium/$IMAGE:latest"
+    fi
 fi
